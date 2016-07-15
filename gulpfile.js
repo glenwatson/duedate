24d176af501fb0c6a58d8cd161da094c6391cbaf
Francis Bergin <bergin.francis@gmail.com>
2016-07-15 19:24:24 -0400

    Add npm, bower and gulp related files

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var del = require('del');
+var addsrc = require('gulp-add-src');
+var concat = require('gulp-concat');
+var runSequence = require('run-sequence');
+var connect = require('gulp-connect');
+
+var htmlmin = require('gulp-htmlmin');
+
+var csslint = require('gulp-csslint');
+var cleanCSS = require('gulp-clean-css');
+
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+
+var lib = {
+    fonts: [
+        'bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2',
+    ],
+    styles: [
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css',
+    ],
+    scripts: [
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
+        'bower_components/moment/min/moment.min.js',
+        'bower_components/angular/angular.min.js',
+    ],
+};
+
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(lib.fonts)
+    .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('styles', function() {
+    return gulp.src('src/css/*')
+    .pipe(csslint())
+    .pipe(csslint.reporter())
+    .pipe(cleanCSS())
+    .pipe(addsrc.prepend(lib.styles))
+    .pipe(concat('duedate.min.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(uglify())
+    .pipe(addsrc.prepend(lib.scripts))
+    .pipe(concat('duedate.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['src/*.html'], ['html']);
+    gulp.watch(['src/css/*', lib.styles], ['styles']);
+    gulp.watch(['src/js/*', lib.scripts], ['scripts']);
+});
+
+gulp.task('connect', function() {
+    connect.server({root: 'dist'});
+});
+
+gulp.task('serve', ['default'], function(cb) {
+    return runSequence(['connect', 'watch'], cb);
+});
+
+gulp.task('default', ['clean'], function(cb) {
+    return runSequence(['html', 'fonts', 'styles', 'scripts'], cb);
+});
